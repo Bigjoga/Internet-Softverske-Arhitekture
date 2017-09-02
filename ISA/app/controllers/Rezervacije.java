7e719a190d000bb86fb84105673adc4bb3261df6
Bigjoga <gajicnikola41@gmail.com>
2017-09-02 12:43:02 +0200

    Odradjeno otkazivanje rezervacije.

--- ISA/app/controllers/Rezervacije.java
+++ ISA/app/controllers/Rezervacije.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import models.Korisnik;
@@ -11,12 +12,34 @@ public class Rezervacije extends Controller{
 
 	public static void show(String mode, Long selectedIndex)
 	{
+		if(session.isEmpty())
+		{
+			redirect("http://localhost:9000/logovanje/show");
+		}
+		
 		List<Restoran> restorani = Restoran.findAll();
 		List<Korisnik> korisnici = Korisnik.findAll();
 		List<Rezervacija> rezervacije = Rezervacija.findAll();
+		List<Rezervacija> listaRezervacijaZaPrikaz = new ArrayList<>();
+		
+		for(int i=0; i<rezervacije.size(); i++)
+		{
+			if(rezervacije.get(i).korisnik.email.equals(session.get("email")))
+			{
+				listaRezervacijaZaPrikaz.add(rezervacije.get(i));
+			}
+		}
+		
 		if(mode == null || mode.equals(""))
 			mode = "edit";
-		render(restorani,korisnici,rezervacije,mode,selectedIndex);
+		
+		render(restorani,korisnici,listaRezervacijaZaPrikaz,mode,selectedIndex);
 	}
 	
+	public static void delete(Long id)
+	{
+		Rezervacija rez = Rezervacija.findById(id);
+		rez.delete();
+		show("edit", rez.id-1);
+	}
 }
