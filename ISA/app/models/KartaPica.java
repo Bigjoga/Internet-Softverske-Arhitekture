c8b531d59bae4568474044a775bf0f0f01728c7e
Bigjoga <gajicnikola41@gmail.com>
2017-09-04 19:21:22 +0200

    Napravljena karta pica i pice.

---
+++ ISA/app/models/KartaPica.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import play.db.jpa.Model;
+
+@Entity
+@Table(name="Karta_pica")
+public class KartaPica extends Model{
+
+	@Column(name = "Naziv_karte_pica", unique = false, nullable = false)
+	public String nazivKartePica;
+	
+	@ManyToOne
+	public Restoran restoran;
+	
+}
---
+++ ISA/app/models/Pice.java
@@ -0,0 +1,26 @@
+package models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import play.db.jpa.Model;
+
+@Entity
+@Table(name="Pice")
+public class Pice extends Model {
+
+	@Column(name = "Naziv_pica", unique = false, nullable = false)
+	public String nazivPica;
+	
+	@Column(name = "Opis_pica", unique = false, nullable = false)
+	public String opisPica;
+	
+	@Column(name = "Cena_karte_pica", unique = false, nullable = false)
+	public Double cena;
+
+}
