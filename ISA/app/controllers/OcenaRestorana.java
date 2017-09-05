807dc5dcb01fb1746fcd22b4b7ce92ec8578065b
DjOka1914 <muticdjordje@gmail.com>
2017-09-05 20:33:21 +0200

    Update OcenaRestorana.java

--- ISA/app/controllers/OcenaRestorana.java
+++ ISA/app/controllers/OcenaRestorana.java
@@ -1,1 +1,39 @@
+package controllers;
 
+import java.util.List;
+
+import models.Restoran;
+
+import play.mvc.Controller;
+
+public class OcenaRestorana extends Controller{
+
+	public static void show(String mode, Long selectedIndex)
+	{
+    
+		if(session.isEmpty())
+      
+		{   
+			 
+      redirect("http://localhost:9000/logovanje/show");
+      
+		}
+		
+		List<Restoran> restorani = Restoran.findAll();
+		
+		if(mode == null || mode.equals(""))
+			mode = "edit";
+    
+		render(restorani,mode,selectedIndex);
+    
+	}
+	
+	public static void edit(Restoran restoran)
+	{ 
+    
+		restoran.save();
+		show("add", restoran.id);
+    
+	}
+	
+}
